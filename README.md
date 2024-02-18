72a28c40cad80724c75e56f8a3f061857de26260
Marco Antonio <marco_281gam@live.co.uk>
2024-02-18 10:32:13 -0300

    safe password complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Boas-vindas ao repositório do projeto Safe Password
+
+Nesse projeto desenvolvi um gerador de senhas seguras que ao clicar no botão para gerar senhas o resultado de senha gerada fica copiado.
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
+import './style.css';
+import copy from 'clipboard-copy';
+
+
 
 const passwordBtnEl = document.querySelector('button');
 const displayPasswordEl = document.querySelector('h2');
 
 passwordBtnEl.addEventListener('click', () => {
-  const randomPassword = nanoid();
-  displayPasswordEl.innerHTML = randomPassword;
+    const randomPassword = nanoid();
+    displayPasswordEl.innerHTML = randomPassword;
+    copy(randomPassword);
+    alert('Senha copiada!');
 });
